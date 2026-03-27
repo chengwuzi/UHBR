a9f2204cb2ef097a9d797f6a524f37c36aca1995
chenglong <2258865762@qq.com>
2026-03-27 13:43:06 +0800

    My_UHBR-调参优化：set_seed方法优化

--- UHBR_single.py
+++ UHBR_single.py
@@ -448,11 +448,16 @@ def test(model, loader, device, metrics):
     return metrics
 
 def set_seed(seed):
+    import random
+    import numpy as np
+    random.seed(seed)
+    np.random.seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = True
+        torch.backends.cudnn.benchmark = False
 
 def print_summary(best_record):
     if not best_record:
